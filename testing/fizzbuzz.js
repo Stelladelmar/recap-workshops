71c9fe8974cf682fae300809f7fffa44f8314f8a
Stella Del Mar <113214552+Stelladelmar@users.noreply.github.com>
2023-04-18 21:00:15 +0100

    stella-doves
    not finished

--- testing/fizzbuzz.js
+++ testing/fizzbuzz.js
@@ -1,3 +1,20 @@
-function fizzbuzz(number) {}
+function fizzbuzz(number) {
+
+    if (number === 1 ) {
+        return "1"
+        
+    }if (number === 2 ) {
+        return "1, 2"
+        
+    }else {
+        return null
+
+    }
+
+}
+
+
+
+
 
 export default fizzbuzz;
--- testing/fizzbuzz.test.js
+++ testing/fizzbuzz.test.js
@@ -8,3 +8,15 @@ test("it should return 1 when passed 1 as an argument", () => {
   // assert
   expect(result).toEqual("1");
 });
+
+// ------------------------------------------
+
+
+test("it should return 1 and 2 when passed 2 as an argument", () => {
+  // arrange
+  let number = 2;
+  // act
+  let result = fizzbuzz(number);
+  // assert
+  expect(result).toEqual("1, 2");
+});
